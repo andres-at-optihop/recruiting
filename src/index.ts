9e1a67442a55a9a9d844c62975d2225b7dab152e
Andres Torres <andres@opithop.io>
2022-01-27 00:05:48 -0500

    Add problem instructions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
+/*
+Dada una lista de números que puede contener duplicados,
+devuelva todos los subconjuntos posibles.
+
+El conjunto de soluciones no debe contener subconjuntos duplicados.
+Devuelva la solución en cualquier orden.
+
+Ejemplo 1:
+
+Entrada: números = [1,2,2]
+Salida: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+
+Ejemplo 2:
+
+Entrada: números = [0]
+Salida: [[],[0]]
+ 
+
+Restricciones:
+
+1 <= nums.longitud <= 10
+-10 <= números[i] <= 10
+*/
+
 export default class Solution {
   getSubSets(nums: number[]): number[][] {
     var result: number[][] = [];
